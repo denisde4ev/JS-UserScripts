708932d3b1555f11e0873eccf02c2a6c30d925b7
Denis Dechev <denisde4ev@vivaldi.net>
2022-11-05 22:03:51 +0200

    old Change: 2022-04-08

--- keyboard-event-hook.js
+++ keyboard-event-hook.js
@@ -5,7 +5,9 @@
 // @description  disalow some keybindings to be handled by the sites
 // @author       denisde4ev
 // @include      http://*.*/*
-// @include      https://*.*/*
+// @include      https://duckduckgo.com/*
+// @include      https://youtube.com/*
+// @exclude      https://youtube.com/tv*
 // @downloadURL  https://github.com/denisde4ev/JS-UserScripts/raw/master/keyboard-event-hook.js
 // @updateURL    https://github.com/denisde4ev/JS-UserScripts/raw/master/keyboard-event-hook.js
 // @grant        none
@@ -16,13 +18,17 @@
 
 
 var documentaddEventListener = document.addEventListener;
+var removeEventListener      = document.removeEventListener;
 
 var fn_apply = Function.call.bind(Function.apply);
+var wm = new WeakMap();
 
-window.document.addEventListener = function(on,fn, opt){
-	if (on !== 'keydown' || typeof fn !== 'function') return fn_apply(documentaddEventListener, this, arguments);
+window.document.addEventListener = function(on, fn, opt) {
+	if (!( on === 'keydown' && typeof fn === 'function' )) {
+		return fn_apply(documentaddEventListener, this, arguments);
+	}
 
-	documentaddEventListener.call(this, on, function(event){
+	var wrap_fn = function(event) {
 
 		if (!event.metaKey && !event.ctrlKey && !event.shiftKey && event.altKey) { // only alt(Alt-) mod key pressed
 			if (event.key === 'ArrowUp') return true; // Alt-ArrowUp
@@ -31,9 +37,20 @@ window.document.addEventListener = function(on,fn, opt){
 
 
 		return fn_apply(fn, this, arguments);
-	}, opt);
+	};
+	wm.set(fn, fn_wrap);
+
+	documentaddEventListener.call(this, on, wrap_fn, opt);
+	
 }
+document.removeEventListener = function (on, fn, opt) {
+	if (wm.has(fn)) {
+		var wrap_fn = wm.get(fn);
+		removeEventListener.call(this, on, wrap_fn, opt);
+	}
+	removeEventListener.call(this, on, fn, opt);
+};
 
-// TODO: remove event listener
 
-window.document.addEventListener.toString = function(){ return documentaddEventListener.toStrig(); } // it's not me
+window.document.addEventListener   .toString = function(){ return documentaddEventListener.toStrig(); }
+window.document.removeEventListener.toString = function(){ return removeEventListener     .toStrig(); }
